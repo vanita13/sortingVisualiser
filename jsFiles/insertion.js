5bc4e855a14b250d85ddac8683b9cd185acfbb40
vanita Bhanushali <vidhibhanushali13@gmail.com>
2021-03-20 17:48:41 +0530

    insertion and selection sort

---
+++ jsFiles/insertion.js
@@ -0,0 +1,34 @@
+
+async function insertionSort(){
+    var x = document.getElementById("bars").querySelectorAll(".flex-child");
+    var i,j,key,keyNode;
+    for(i=1 ;i < x.length;i++){
+        keyNode = x[i];
+        key = parseInt(x[i].style.height.match(/\d/g).join(''));
+        j = i -1;
+        while (j >= 0 && parseInt(x[j].style.height.match(/\d/g).join('')) > key)
+        { 
+            //console.log(x[j]+" "+x[j].style.height+x[j+1]+" "+x[j+1].style.height);
+            x[j].style.background="red";
+            x[j+1].style.background="red";
+            await new Promise(resolve => setTimeout(() => {resolve(), 200000}));
+            x[j + 1].style.height = x[j].style.height
+            await new Promise(resolve => setTimeout(() => {resolve(), 200000}));
+            x[j].style.background="yellow";
+            x[j+1].style.background="yellow";
+            //console.log(x[j]+" "+x[j].style.height+x[j+1]+" "+x[j+1].style.height);
+            //console.log(x[j+1].style.height);
+            //arr[j + 1].style.height = arr[j].style.height; 
+            j = j - 1; 
+        } 
+        //swap(x[j+1],keyNode);
+        x[j+1].style.background="red";
+        x[i].style.background="red";
+        await new Promise(resolve => setTimeout(() => {resolve(), 200000}));
+        x[ j+ 1].style.height = key+"px"; 
+        await new Promise(resolve => setTimeout(() => {resolve(), 200000}));
+        x[i].style.background="yellow";
+        x[j+1].style.background="yellow";
+
+    }
+}
---
+++ jsFiles/selection.js
@@ -0,0 +1,36 @@
+function swap(el1,el2)
+{
+
+    const style1 = window.getComputedStyle(el1);
+    const style2 = window.getComputedStyle(el2);
+
+
+    const transform1 = style1.getPropertyValue("height");
+    const transform2 = style2.getPropertyValue("height");
+
+    el1.style.height = transform2;
+    el2.style.height = transform1;
+
+ }
+
+async function selectionSort(){
+    var x = document.getElementById("bars").querySelectorAll(".flex-child");
+    var i,j,minIndex;
+    for(i =0;i<x.length-1;i++){
+        minIndex = i;
+        for(j = i+1;j<x.length;j++){
+            var h1 = parseInt(x[j].style.height.match(/\d/g).join(''));
+            var h2 = parseInt(x[minIndex].style.height.match(/\d/g).join(''));
+            if(h1<h2){
+                minIndex = j;
+            }
+        }
+        x[i].style.background="red";
+        x[minIndex].style.background="red";
+        await new Promise(resolve => setTimeout(() => {resolve(), 200000}));
+        swap(x[i],x[minIndex]);
+        await new Promise(resolve => setTimeout(() => {resolve(), 200000}));
+        x[i].style.background="yellow";
+        x[minIndex].style.background="yellow";
+    }
+}
