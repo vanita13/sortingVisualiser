026b93390c28832dc1a98d7bc660e33a2069d46e
Vanita13 <vidhibhanushali13@gmail.com>
2024-01-09 13:26:37 +0530

    quick sort added

--- jsFiles/quick.js
+++ jsFiles/quick.js
@@ -1,87 +1,62 @@
 function swap(el1,el2)
 {
-
-    const style1 = window.getComputedStyle(el1);
-    const style2 = window.getComputedStyle(el2);
-
-
-    const transform1 = style1.getPropertyValue("height");
-    const transform2 = style2.getPropertyValue("height");
-
-    el1.style.height = transform2;
-    el2.style.height = transform1;
+    //setTimeout((el1,el2)=>{el1.style.background="red"; el2.style.background="red";},200000)
+	temp = el1.style.height
+    el1.style.height = el2.style.height;
+    el2.style.height = temp;
+    //setTimeout((el1,el2)=>{el1.style.background="red"; el2.style.background="red";},200000)
 
  }
-// function swap1(items, leftIndex, rightIndex){
-//     var temp = items[leftIndex].style.height;
-//     items[leftIndex].style.height = items[rightIndex].style.height;
-//     items[rightIndex].style.height = temp;
-// }
-//  function partition(items, left, right) {
-//     var pivot   = items[Math.floor((right + left) / 2)].style.height, //middle element
-//         i       = left, //left pointer
-//         j       = right; //right pointer
-//     while (i <= j) {
-//         while (items[i].style.height < pivot) {
-//             i++;
-//         }
-//         while (items[j].style.height > pivot) {
-//             j--;
-//         }
-//         if (i <= j) {
-//             swap1( items,i,j); //sawpping two elements
-//             i++;
-//             j--;
-//         }
-//     }
-//     return i;
-// }
 
-// function quickSort(items, left, right) {
-//     var index;
-//     if (items.length > 1) {
-//         index = partition(items, left, right); //index returned from partition
-//         if (left < index - 1) { //more elements on the left side of the pivot
-//             quickSort(items, left, index - 1);
-//         }
-//         if (index < right) { //more elements on the right side of the pivot
-//             quickSort(items, index, right);
-//         }
-//     }
-//     //return items;
-// }
 
-function partition (arr ,low ,high) 
-{
-    var pivot = arr[high].style.height; // pivot 
-    var i = (low -1); // Index of smaller element and indicates the right position of pivot found so far
- 
-    for (var j = low; j <= high -1; j++) 
-    {
-        // If current element is smaller than the pivot 
-        if (arr[j].style.height < pivot) 
-        { 
-            i++; // increment index of smaller element 
-            swap(arr[i], arr[j]); 
-        } 
-    } 
-    swap(arr[i + 1], arr[high]); 
-    return (i + 1); 
-} 
+// Function to partition the array and return the partition index
+function partition(arr, low, high) {
+    
+	// Choosing the pivot
+	let pivot = parseFloat(arr[high].style.height);
 
-function quickSort(arr, low, high) 
-{ console.log("low"+low+"heigh"+high);
-    if (low < high) 
-    { 
-        /* pi is partitioning index, arr[p] is now 
-        at right place */
-        var pi = partition(arr, low, high); 
-        console.log(pi) ;
- 
-        // Separately sort elements before 
-        // partition and after partition 
-        quickSort(arr, low, pi -1); 
-        quickSort(arr, pi +1, high); 
-    } 
-    console.log("low"+low+"heigh"+high);
-} 
+	// Index of smaller element and indicates the right position of pivot found so far
+	let i = low - 1;
+	for (let j = low; j <= high - 1; j++) {
+        function changeColor(){
+            arr[i].style.background="yellow"; arr[j].style.background="yellow"
+        }
+        function changeColorRed(){
+            arr[i].style.background="red"; arr[j].style.background="red"
+        }
+		// If current element is smaller than the pivot
+        let h1 = parseFloat(arr[j].style.height);
+		if (h1 <= pivot) {
+			// Increment index of smaller element
+			i++;
+            //setTimeout((el1,el2)=>{el1.style.background="red"; el2.style.background="red";},200000)
+            //setTimeout(()=>{changeColorRed;},200);
+            swap(arr[i],arr[j]);
+            //setTimeout(changeColor,300);
+            
+        };
+            
+		}
+        
+	
+
+    swap(arr[i+1],arr[high]);
+	//[arr[i + 1], arr[high]] = [arr[high], arr[i + 1]]; // Swap pivot to its correct position
+	return i + 1; // Return the partition index
+}
+
+// The main function that implements QuickSort
+function quickSort(arr, low, high) {
+	if (low < high) {
+		// pi is the partitioning index, arr[pi] is now at the right place
+		let pi = partition(arr, low, high);
+
+		// Separately sort elements before partition and after partition
+        console.log("Calling quicksot again")
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+
+
